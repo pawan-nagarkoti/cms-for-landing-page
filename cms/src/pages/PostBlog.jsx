b7cb477d3d1d58a336750fd3c34a14368b6cce92
Pawan <pawan@Pawans-MacBook-Air.local>
2025-01-12 15:23:12 +0530

    added logic for listing item in the array

--- cms/src/pages/PostBlog.jsx
+++ cms/src/pages/PostBlog.jsx
@@ -18,6 +18,9 @@ export default function PostBlog() {
   const [navItem, setNavItem] = useState(null);
   const [navitemList, setNavItemList] = useState([]);
 
+  const [listItem, setListItem] = useState(null);
+  const [storedNavListItem, setStoredNavListItem] = useState([]);
+
   const handleChange = (key) => (e) => {
     const value = e.target.type === "file" ? e.target.files[0] : e.target.value;
     setFormData({ ...formData, [key]: value });
@@ -26,12 +29,18 @@ export default function PostBlog() {
   const handleSubmit = (e) => {
     e.preventDefault();
     formData.headerMenu = navitemList;
+    formData.listing = storedNavListItem;
     console.log(formData); // Logs the collected data
   };
 
   const handleAddMenuItem = () => {
     setNavItemList((pre) => [...pre, navItem]);
     setNavItem("");
+  };
+
+  const handleAddListItem = () => {
+    setStoredNavListItem((prev) => [...prev, listItem]);
+    setListItem("");
   };
   return (
     <>
@@ -73,14 +82,30 @@ export default function PostBlog() {
             placeholder="enter your short description"
             onChange={handleChange("headingOneContent")}
           />
+
           <CustomInput label="Heading Two" type="text" placeholder="enter your title" onChange={handleChange("headingTwo")} />
           <div className="flex items-center space-x-4">
-            <CustomInput label="Listing" type="text" placeholder="List" />
-            <button className="mt-3 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2">
+            <CustomInput label="Listing" type="text" placeholder="List" value={listItem ?? ""} onChange={(e) => setListItem(e.target.value)} />
+            <button
+              type="button"
+              onClick={handleAddListItem}
+              className="mt-3 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+            >
               Add
             </button>
           </div>
-          <CustomInput label="Heading Two Images" type="text" placeholder="enter your title" onChange={handleChange("headingTwoImages")} />
+
+          {storedNavListItem.length > 0 && (
+            <ul className="list-disc list-inside space-y-2 bg-gray-100 p-4 rounded-md shadow-md mb-3">
+              {storedNavListItem.map((v, i) => (
+                <li key={i} className="text-gray-800 text-sm font-medium">
+                  {v}
+                </li>
+              ))}
+            </ul>
+          )}
+
+          <CustomUploadImage label="Heading Two Images" onChange={handleChange("headingTwoImages")} />
           <CustomUploadImage label="Gallery" />
           <div className="flex items-center space-x-4">
             <CustomInput label="Amenties" type="text" placeholder="amenities name" />
