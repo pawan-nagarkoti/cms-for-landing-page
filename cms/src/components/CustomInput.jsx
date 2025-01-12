75ddfd608f6eb7020e9670f717552b23f9c6172c
Pawan <pawan@Pawans-MacBook-Air.local>
2025-01-12 08:31:56 +0530

    added components for input and textarea

--- cms/src/components/CustomInput.jsx
+++ cms/src/components/CustomInput.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { useId } from "react";
 
-export default function CustomInput() {
-  return <div>CustomInput</div>;
+export default function CustomInput({ label = "", type = "text", ...props }) {
+  const { id } = useId;
+  return (
+    <div className="mb-4">
+      <label htmlFor={id} className="block text-sm font-medium text-gray-700 mb-2">
+        {label}
+      </label>
+      <input
+        type={type}
+        name=""
+        id={id}
+        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        {...props}
+      />
+    </div>
+  );
 }
--- cms/src/pages/PostBlog.jsx
+++ cms/src/pages/PostBlog.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { CustomInput } from "../components";
+import { CustomInput, CustomTextarea } from "../components";
 
 export default function PostBlog() {
   return (
     <>
-      <CustomInput />
+      <div className="container mx-auto px-4 md:px-8 lg:px-16 py-6">
+        <h1 className="text-4xl md:text-5xl font-bold text-center text-gray-800 my-6">Landing Page Post</h1>
+
+        <form>
+          <CustomInput label="Title" type="text" placeholder="enter your title" />
+          <CustomTextarea label="Short Description" placeholder="enter your short description" />
+          <div className="mb-2">
+            <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
